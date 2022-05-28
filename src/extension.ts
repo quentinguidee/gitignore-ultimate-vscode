49761007fe290eda3eb80b83ab82012f67a9edec
Quentin Guidée <quentin.guidee@gmail.com>
2022-05-28 19:17:08 +0200

    Add back commands to add files to .gitignore (closes #5)
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,23 @@
-import { ExtensionContext } from "vscode";
+import { commands, ExtensionContext } from "vscode";
 import { LanguageClient } from "vscode-languageclient/node";
 import { createClient, stopClient } from "./client";
+import { addFileToGitignore, createGitignoreFile } from "./features/commands";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
     client = createClient(context);
     client.start();
+
+    commands.registerCommand(
+        "gitignore-ultimate.create-gitignore",
+        createGitignoreFile
+    );
+
+    commands.registerCommand(
+        "gitignore-ultimate.add-to-gitignore",
+        addFileToGitignore
+    );
 
     console.log("GitIgnore Ultimate is now active.");
 }
---
+++ src/features/commands.ts
@@ -0,0 +1,33 @@
+import { appendFile, existsSync, writeFile } from "fs";
+import { join, relative } from "path";
+import { Uri, window, workspace } from "vscode";
+
+export async function createGitignoreFile(uri: Uri) {
+    const path = join(uri.path, ".gitignore");
+
+    if (existsSync(path)) {
+        window.showInformationMessage("A .gitignore file already exists.");
+        return;
+    }
+
+    writeFile(path, "", () => {
+        showTextDocument(path);
+    });
+}
+
+export function addFileToGitignore(uri: Uri) {
+    const root = workspace.getWorkspaceFolder(uri);
+    if (!root) return;
+    const content = "\n" + relative(root.uri.fsPath, uri.fsPath);
+    const path = Uri.file(join(root.uri.fsPath, ".gitignore")).fsPath;
+
+    appendFile(path, content, () => {
+        showTextDocument(path);
+    });
+}
+
+function showTextDocument(path: string) {
+    workspace.openTextDocument(path).then((textDocument) => {
+        window.showTextDocument(textDocument);
+    });
+}
